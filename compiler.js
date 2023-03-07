4d810f1d315510d4936904bb624c640867afa6b2
XiaoRik <xxd0225@gmail.com>
2023-03-07 09:58:49 +0800

    add genearte code

--- compiler.js
+++ compiler.js
@@ -1,6 +1,7 @@
 const tokenizer = require('./tokenizer');
 const parser = require('./parser');
 const transformer = require('./transformer');
+const generateCode = require('./generateCode');
 module.exports = function compiler(input) {
   // 1. Lexical Analysis -
   //      Breaks the input code (string) into the basic syntax
@@ -10,9 +11,11 @@ module.exports = function compiler(input) {
   //      Transforms the tokens (array of objects) into an
   //      AST (tree of objects) which represents our program
   const lispAST = parser(tokens);
-  // 3. Transformation
+  // 3. Transformation - Transforms our original Lisp AST into
+  //                     our target Javascript AST
   const jsAST = transformer(lispAST);
-  // 4. Code Generation
-  //
-  return jsAST;
+  // 4. Code Generation - Transforms our target AST (object of objects)
+  //                      into actual code (string)
+  const jsCode = generateCode(jsAST);
+  return jsCode;
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const compiler = require('./compiler');
 const input = '(add 2 (sub 4 3))';
 const output = compiler(input);
-json_output = JSON.stringify(output,null,2);
-console.log(json_output);
+// json_output = JSON.stringify(output,null,2);
+console.log(output);
 // add(2, sub(4,3)); // javascript
 // (add 2 (sub 4 3)) // list
